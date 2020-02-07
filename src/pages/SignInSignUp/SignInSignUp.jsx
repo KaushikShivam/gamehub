457faf39d846d1475ca1788cd10da98a861f8ce7
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 23:28:35 +0530

    Add Custom FormInput and styles

--- src/pages/SignInSignUp/SignInSignUp.jsx
+++ src/pages/SignInSignUp/SignInSignUp.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import SignIn from './../../components/SignIn/SignIn';
+
 import './SignInSignUp.scss';
 
 const SignInSignUp = () => {
-  return <div className="sign-in-sign-up"></div>;
+  return (
+    <div className="sign-in-sign-up">
+      <SignIn />
+    </div>
+  );
 };
 
 export default SignInSignUp;
