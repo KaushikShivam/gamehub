e9040657179ff2dda264bbe25577106b16a3ed77
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-08 04:28:16 +0530

    Add user action

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
 import './index.css';
 import App from './App';
-
-import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
