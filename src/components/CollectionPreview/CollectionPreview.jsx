4eba6cb0a1735a06e9c389d9139988f47faccc42
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 19:39:07 +0530

    Add collectionItem and Collectionitem preview

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CollectionItem from './../CollectionItem/CollectionItem';
+
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => {
@@ -9,8 +11,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map(item => (
-            <div key={item.id}>{item.name}</div>
+          .map((id, ...otherItemProps) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
