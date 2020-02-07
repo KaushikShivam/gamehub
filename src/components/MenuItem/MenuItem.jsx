7e690cad85bea0fafd699dcc58f893a4245cdd06
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 18:06:55 +0530

    Render menu item size conditionally

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import './MenuItem.scss';
 
-const MenuItem = ({ title, imageUrl }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
+  console.log(size);
   return (
-    <div style={{ backgroundImage: `url(${imageUrl})` }} className="menu-item">
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className={`${size} menu-item`}
+    >
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">BUY NOW</span>
