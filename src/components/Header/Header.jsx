db28fdd22046c4a7f1d184f4b96b2159ab3ead67
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-08 02:32:05 +0530

    Add user auth state to app

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ const Header = ({ currentUser }) => {
         </Link>
         {currentUser ? (
           <div className="option" onClick={() => auth.signOut()}>
-            Sign Out
+            SIGN OUT
           </div>
         ) : (
           <Link className="option" to="/signin">
