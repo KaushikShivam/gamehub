5b9f40f7c28c491be2dca0113cdd21985c407c8b
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 18:44:52 +0530

    Implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/HomePage/HomePage';
 
+const Hats = () => <h1>Hats</h1>;
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
