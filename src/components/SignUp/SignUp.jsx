4353c2bb2267f23eab15cd2ab04e9798ddaf01bf
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-08 02:37:10 +0530

    Create signup component

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+import './SignUp.scss';
+
+import FormInput from './../FormInput/FormInput';
+import CustomButton from './../CustomButton/CustomButton';
+import {
+  auth,
+  createUserProfileDocument
+} from './../../firebase/firebase.utils';
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   this.setState({ email: '', password: '' });
+  // };
+
+  // handleChange = e => {
+  //   const { value, name } = e.target;
+  //   this.setState({ [name]: value });
+  // };
+
+  render() {
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="displayName"
+            type="text"
+            value={displayName}
+            handleChange={this.handleChange}
+            label="Display Name"
+            required
+          />
+          <FormInput
+            name="email"
+            type="email"
+            value={email}
+            handleChange={this.handleChange}
+            label="Email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={password}
+            handleChange={this.handleChange}
+            label="Password"
+            required
+          />
+          <FormInput
+            name="confirmPassword"
+            type="password"
+            value={confirmPassword}
+            handleChange={this.handleChange}
+            label="Confirm Password"
+            required
+          />
+
+          <CustomButton type="submit">Sign Up</CustomButton>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignUp;
