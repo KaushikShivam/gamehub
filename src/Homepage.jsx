b9c1b71eefdb8f2c74f266bcca1365bbdf8fb25d
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 17:28:30 +0530

    Create homepage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ACTION</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">ARCADE</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">STRATEGY</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">CONSOLES</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">PC</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
