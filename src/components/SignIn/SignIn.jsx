e66c375b0d5cccfee377d76c517f33e94385fdc9
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-07 23:17:15 +0530

    Add form change and handle logic

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -10,19 +10,37 @@ class SignIn extends Component {
       password: ''
     };
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+
   render() {
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form>
-          <input name="email" type="email" value={this.state.email} required />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
           <label>Email</label>
           <input
             name="password"
             type="password"
             value={this.state.password}
+            onChange={this.handleChange}
             required
           />
           <label>Password</label>
