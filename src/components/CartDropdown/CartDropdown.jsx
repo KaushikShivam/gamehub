a171a9330ed5fe6d3411b65ee01ffd450a2a413d
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-08 22:01:53 +0530

    Add cart dropdown button

---
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CustomButton from './../CustomButton/CustomButton';
+import './CartDropdown.scss';
+
+const CartDropdown = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items"></div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
