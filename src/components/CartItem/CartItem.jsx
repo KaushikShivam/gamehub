9798b847e9e77c0ef92ba36f9974cb6ca82f49f1
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-09 14:04:25 +0530

    Add CartItem styles

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './CartItem.scss';
 
-const CartItem = ({ item: { imageUrl, price, name } }) => {
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
     <div className="cart-item">
       <img src={imageUrl} alt="item" />
       <div className="item-detail">
         <span className="name">{name}</span>
-        <span className="price">${price}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
       </div>
     </div>
   );
