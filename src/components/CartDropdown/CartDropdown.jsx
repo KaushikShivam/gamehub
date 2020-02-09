6e3bb059862b9844664cc5af24701500159d38c3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-09 14:08:06 +0530

    Configure CartDropdown with redux cartitems

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from './../CustomButton/CustomButton';
+import CartItem from './../CartItem/CartItem';
+
 import './CartDropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items"></div>
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = state => ({
+  cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -6,7 +6,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
     <div className="cart-item">
       <img src={imageUrl} alt="item" />
-      <div className="item-detail">
+      <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
           {quantity} x ${price}
