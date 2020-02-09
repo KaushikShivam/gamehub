bb510b1d5a9a619c258f443ad5cdda5d55c580d7
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-09 15:37:45 +0530

    Add reselect to meoize

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -4,6 +4,7 @@ import { ReactComponent as ShoppingIcon } from './../../assets/shopping-bag.svg'
 import './CartIcon.scss';
 
 import { toggleCartHidden } from './../../redux/cart/cart.actions';
+import { selectCartItemsCount } from './../../redux/cart/cart.selectors';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
@@ -18,11 +19,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0
-  )
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
