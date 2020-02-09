a600d48e60f85bd1a35179b92e877403b524f8a8
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-09 17:26:12 +0530

    Add Checkout styles

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 import './CheckOut.scss';
 
 const CheckOut = () => {
-  return <div> CHECKOUT PAGE</div>;
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
