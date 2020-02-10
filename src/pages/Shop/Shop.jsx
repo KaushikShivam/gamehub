a0222b3820b27061f8459bfe84b984ec07d53d2d
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-10 19:54:54 +0530

    Refactor shop collections to reduc state

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
-
-import SHOP_DATA from './shopData';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollections } from './../../redux/shop/shop.selectors';
 
 import CollectionPreview from './../../components/CollectionPreview/CollectionPreview';
 
-class Shop extends Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
 
-export default Shop;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+
+export default connect(mapStateToProps)(Shop);
