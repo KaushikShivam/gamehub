dcf3c0f0fca4fd867bbb3275dc26df4b4236a3c3
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-13 21:01:39 +0530

    Clear cart saga

---
+++ src/redux/cart/cart.sagas.js
@@ -0,0 +1,16 @@
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import UserActionTypes from '../user/user.types';
+
+import { clearCart } from './cart.actions';
+
+export function* clearCartOnSuccess() {
+  yield put(clearCart());
+}
+
+export function* onSignOutSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSuccess);
+}
+
+export function* cartSagas() {
+  yield all([call(onSignOutSuccess)]);
+}
