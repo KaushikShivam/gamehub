09507ece499220e7feb83501df8bcc31e28ffdff
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-13 17:55:31 +0530

    Move thunk shop action into saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,8 +1,34 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
 import ShopActionTypes from './shop.types';
+import {
+  firestore,
+  convertCollectionSnapshotToMap
+} from './../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shop.actions';
 
 function* fetchCollectionsAsync() {
   yield console.log('I am fired');
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+
+  // collectionRef
+  //   .get()
+  //   .then(snapshot => {
+  //     const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch(error => dispatch(fetchCollectionsFailure(error)));
 }
 
 export function* fetchCollectionsStart() {
