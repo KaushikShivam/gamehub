de901d3d0727efd39dfa614e95970431cd9dd0cb
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-14 15:50:39 +0530

    Finish implementing Payment

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_JcziI8f3be7vZIDrHGc3Kzza00O3PJGIcY';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment successfull');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+      .then(response => {
+        alert('Payment was successfull');
+      })
+      .catch(error => {
+        console.log('There was some payment error', error);
+        alert(
+          'There was some issue with the payment. Please use the provided card'
+        );
+      });
   };
 
   //TODO: Change the image
