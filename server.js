69cc1f4116ad13d8f53381697714adefd04781ea
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-14 15:42:07 +0530

    Add basic stripe implementation

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -23,4 +25,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(PORT, error => {
   if (error) throw error;
   console.log('Server running on port ' + PORT);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeError, stripeRes) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
