fd4d9cb1a94c7aa0520cd225ae4f6588b5dcbff4
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-18 13:52:46 +0530

    Fix homepage padding

--- client/src/pages/Homepage/Homepage.styles.jsx
+++ client/src/pages/Homepage/Homepage.styles.jsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
 
-export const HomepageContainer = styled.div`
+export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 20px 80px;
 `;
