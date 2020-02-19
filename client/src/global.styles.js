56ab6fee1360e239e30f6346e5828156bd4a06fd
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-19 21:38:52 +0530

    Fix background styles

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -3,7 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 	body {
 		font-family: 'Open Sans Condensed';
-		padding: 20px 40px;
+		padding: 20px 20px;
+    background-color: #132047;
+    color: white;
 
 		@media screen and (max-width: 800px) {
 			padding: 10px;
