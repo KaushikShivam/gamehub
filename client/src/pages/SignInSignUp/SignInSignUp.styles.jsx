bbcbc17ff0d4c79809d7bc625ee2045d0c02c57c
Shivam Kaushik <shivam.kaushik@outlook.com>
2020-02-20 13:12:21 +0530

    Finish styling Signup page

--- client/src/pages/SignInSignUp/SignInSignUp.styles.jsx
+++ client/src/pages/SignInSignUp/SignInSignUp.styles.jsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 export const SignInAndSignUpContainer = styled.div`
-  width: 850px;
+  width: 950px;
   display: flex;
   justify-content: space-between;
   margin: 30px auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 
-  @media screen and (max-width: 800px) {
+  @media screen and (max-width: 950px) {
     flex-direction: column;
     width: unset;
     align-items: center;
+    position: static;
+    transform: translate(0, 0);
 
     > *:first-child {
       margin-bottom: 50px;
